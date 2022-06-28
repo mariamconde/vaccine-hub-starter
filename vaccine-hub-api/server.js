fe69bdd8f2f543f5e2038749e698e146d18afc19
Mariam Conde <Condemriam@gmail.com>
2022-06-28 12:57:40 -0400

    barebones server created

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,18 +1,31 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+const { BadRequestError, NotFoundError } =  require("./utils/errors")
 
 const app = express()
 
 
 // enables cors-orgin resource sharing for all orgins
 app.use(cors())
-
 // parse incoming request bodies with JSON payloads
 app.use(express.json())
-
 // log request info
 app.use(morgan("tiny"))
+
+
+app.use((req, res, next) => {
+    return next(new NotFoundError())
+})
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500
+    const message = err.message
+
+    return res.status(status).json({
+        error: { message, status }
+    })
+})
 
 const PORT = process.env.PORT || 3001
 
